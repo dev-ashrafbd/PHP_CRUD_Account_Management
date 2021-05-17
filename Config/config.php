5e2e0c7006b0653d64507a05f19d8edeb409aabb
Md.Ashraful Islam <ashraful.istiaque@gmail.com>
2021-05-17 22:16:58 +0600

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -1,3 +1,3 @@
 <?php
-
+//mysqli_connect("Server Name", "Username", "Password", "Database Name")
 $con = mysqli_connect("localhost", "root", "", "crud") or die("Can't connect successfully." . mysqli_connect_error());
